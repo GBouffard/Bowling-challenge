373f2109dd63d295f77bc639563fc1e67fbf1d3e
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-07-15 16:25:48 +0100

    written tests for the engine

---
+++ README.md
@@ -0,0 +1,53 @@
+
+:bowling: Bowling Challenge :bowling:
+=================
+
+This project is the week 5 challenge from Makers Academy. The task was to count and sum the scores of a bowling game for one player in JavaScript and make an interface in JQuery.
+
+- A bowling game consists of 10 frames in which the player tries to knock down the 10 pins. 
+-In every frame the player can roll one or two times (or maybe 3 in the last frame).
+- The actual number depends on strikes and spares. 
+- The score of a frame is the number of knocked down pins plus bonuses for strikes and spares. 
+- After every frame the 10 pins are reset. 
+
+You can find the scoring rules on this wikipedia [link](http://en.wikipedia.org/wiki/Ten-pin_bowling). It was suggested that our tests should include:
+-a Gutter Game: a Gutter Game is when the player never hits a pin (20 zero scores).
+-a Perfect Game: A Perfect Game is when the player rolls 12 strikes (10 regular strikes and 2 strikes for the bonus in the 10th frame). The Perfect Game scores 300 points.
+
+Heroku
+----
+[Guillaume's Bowling Game]()
+
+Objectives of exercise
+----
+To learn about Javascript, Jasmine and jQuery.
+
+Technologies used
+----
+- Javascript
+- Jasmine
+- JQuery
+- HTML, ejs & CSS
+- Node.js
+- Heroku
+
+How to run it
+----
+```
+git clone git@github.com:GBouffard/Bowling-challenge.git
+cd Bowling-challenge
+npm install
+node server.js
+```
+By opening your browser on http://localhost:8080/, you can now you can count the score of a game you play.
+
+How to run tests
+----
+```
+cd Bowling-challenge
+open SpecRunner.html
+```
+
+and this is what you should see:
+
+![](public/Jasmine_tests_to_come.png)
--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -4,4 +4,61 @@ describe('Game', function() {
   beforeEach(function() {
     game = new Game();
   });
+  
+  describe('generic rules', function() {
+    xit('starts with a score of 0', function() {
+    });
+
+    xit('tracks the pins that have fallen at each roll', function() {
+    });
+
+    xit('knows which frame we are at each stage', function() {
+    });
+
+    xit('knows that a 2nd roll cannot knock more pins out than what is left after the first roll', function() {
+    });
+
+    xit('knows that it\s not possible to play more than 10 frames', function() {
+    });
+  });
+
+  describe('from frame 1 to 9', function() {
+    xit('knows that a frame is over when a strike happens', function() {
+    });
+  });
+
+  describe('on the 10th frame', function() {
+    xit('knows that a strike on the first roll means an additional 2 rolls to play', function() {
+    });
+
+    xit('knows that a spare on the 2nd roll means an additional roll to play', function() {
+    });
+  });
+
+  describe('total score calculation', function() {
+    xit('calculates the frame score and adds it to the total right away if the sum of that frame is inferior to 10', function() {
+    });
+
+    xit('after a spare, calculates the frame score and adds it to the total only after a spare roll has been made', function() {
+    });
+
+    xit('after a strike, calculates the frame score and adds it to the total only after 2 spare rolls have been made', function() {
+    });
+  });
+
+  describe('gameplay', function() {
+    xit('a player can play a standard game and get the expected score', function() {
+    });
+
+    xit('a player can play a gutter game and get the expected score: a big fat 0!', function() {
+    });
+
+    xit('a player can play a perfect game and get the expected score: an amazing 300!', function() {
+    });
+  });
+
+  describe('to restart a game', function() {
+    xit('a player has the option to reset the total score to 0', function() {
+    });
+  });
 });
