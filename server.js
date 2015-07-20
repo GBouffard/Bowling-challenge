d76c571bd1a07473334dee0e8711d533dc4f510a
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-07-20 14:28:08 +0100

    created a JQuery offline solution

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.set('view engine', 'ejs');
 
 app.use("/public", express.static(__dirname + '/public'));
 app.use("/src", express.static(__dirname + '/src'));
+app.use("/lib", express.static(__dirname + '/lib'));
 
 app.get('/', function(req, res) {
 	res.render('index');
